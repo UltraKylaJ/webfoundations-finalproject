94257d475179b103759054c05fb1ca6ead2600a8
Kyla Joiner <kylajoiner@Kylas-MacBook-Pro.local>
2022-04-29 13:33:14 -0700

    Still figuring this out

---
+++ script.js
@@ -0,0 +1,31 @@
+//Lord Jesus, I pray that I can complete this project in a timely manner. I pray that I understand it and that what I don't understand I will receive help to understand. Please continue to give me the excitement I have been receiving from learning in this program. Thank you for all you have blessed me with and will continue to bless me with.
+
+// Flexbox, CSS Grid, or Bootstrap
+
+// 1. Navbar
+    // Contains a logo (use a dummy logo or create your own)
+    // Links to Home, Resume, Gallery, and Contact (doesn't need to be functional)
+    // All items should be well spaced and organized on the left-hand side of the page
+
+// 2. A full-width panel with two columns containing the following:
+    // Add a background color of choice
+    // Column 1
+        // Your full name (or a fake name of choice)
+        // Title or Aspiring Tile
+        // A brief description
+        // Use DOM methods to create a hover effect to increase the size of the name and title elements
+    // Column 2
+        // An image of yourself or a placeholder image
+
+// 3. A full-width panel containing three columns and the following:
+    // Column 1: Technical Skills (List at least 3 items)
+    // Column 2: Soft Skills (at least 4 items)
+    // Column 3: Hobbies (at least 3 items)
+    // Add a border of choice to each list
+    // Add effects to each column to have a background color change when hovered over
+
+// 4. A full-width panel with the following:
+    // Using AJAX in JavaScript, fetch data from https://api.adviceslip.com/advice and display 
+    // the Random Advice dynamically within the HTML page (https://api.adviceslip.com/)
+
+// 5. A footer with your GitHub link
